3f2b41ed026c5c24014ac860c3327a28cd4c88cd
Yehia148 <yahialsayed@aucegypt.edu>
2025-10-21 21:03:15 +0300

    Node Struct

--- ConcordanceTree.h
+++ ConcordanceTree.h
@@ -4,5 +4,14 @@
 
 #ifndef UNTITLED1_CONCORDANCETREE_H
 #define UNTITLED1_CONCORDANCETREE_H
+#include <iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    int height;
+    int frequency;
+};
 
 #endif //UNTITLED1_CONCORDANCETREE_H
