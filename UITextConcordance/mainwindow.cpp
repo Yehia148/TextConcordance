6c7c0f26523bcfd55def8b5d10a00a080e331fc1
Yehia148 <yahialsayed@aucegypt.edu>
2025-11-02 21:27:18 +0200

    Linking QT to the rest of the project

--- UITextConcordance/mainwindow.cpp
+++ UITextConcordance/mainwindow.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include ".\ui_mainwindow.h"
+#include "..\TrieConcordance.h"
+#include "..\ConcordanceTree.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    ConcordanceTree test1;
+    TrieConcordance test2;
+    test2.showWordInfo("string");
+
     ui->setupUi(this);
 }
 
