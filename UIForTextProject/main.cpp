91d9d3299394c58b93d6abbc27f02e43c6100ec4
Yehia148 <yahialsayed@aucegypt.edu>
2025-11-02 22:47:49 +0200

    Fixing Errors with QT integration

--- UIForTextProject/main.cpp
+++ UIForTextProject/main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
-
+#include "..\TrieConcordance.h"
+#include "..\ConcordanceTree.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    TrieConcordance test1;
+    ConcordanceTree test2;
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
--- UIForTextProject/mainwindow.cpp
+++ UIForTextProject/mainwindow.cpp
@@ -1,16 +1,10 @@
 #include "mainwindow.h"
-#include ".\ui_mainwindow.h"
-#include "..\TrieConcordance.h"
-#include "..\ConcordanceTree.h"
+#include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    ConcordanceTree test1;
-    TrieConcordance test2;
-    test2.showWordInfo("string");
-
     ui->setupUi(this);
 }
 
