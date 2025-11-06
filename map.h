e32d83ee70947d161ce6decb7200749077ff332d
Aya Amr Noaman <79466186+ayanoaman3@users.noreply.github.com>
2025-11-06 21:43:40 +0200

    Updated map.h and map.cpp (To do: testing)

--- map.h
+++ map.h
@@ -1,18 +1,35 @@
 #include <string>
 #include <vector>
 #include <utility>
+// #include "spacy/spacy.h"
 using std::string, std::vector, std::pair;
+
+
+class Node {
+public:
+    string word;
+    int frequency;
+    Node* next;
+    Node(string word, int frequency, Node* next): word(word), frequency(frequency), next(next) {}
+};
 
 
 class Map {
 private:
-    vector<vector<pair<string, int> > > frequency_list;
+    Node** frequency_list;
+    const int LIST_LENGTH = 2503;
+    int word_count;
     int hash_fun(string token);
+    vector<pair<string, int> > sort_by_frequency();
+    void append_word(string word, int hash);
+    void merge_sort(vector<pair<string, int> > list);
 
 public:
-    Map(int word_count);
-    Map(int word_count, string token);
-    Map(vector<string> token_list);
-    void put_token(string token);
-    int get_frequency(string token);
+    Map();
+    Map(string word);
+    Map(string word_list[], int word_count);
+    Map(vector<string> word_list);
+    void put_word(string word);
+    int delete_word(string word);
+    int get_frequency(string word);
 };
