0bca5b9dba593770a0c6b530e38bc5fa83d248a3
Yehia148 <yahialsayed@aucegypt.edu>
2025-11-09 20:04:45 +0200

    pushbutton implementations

--- UIForTextProject/mainwindow.cpp
+++ UIForTextProject/mainwindow.cpp
@@ -1,12 +1,15 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include "..\TrieConcordance.h"
-#include "..\ConcordanceTree.h"
+//#include "..\map.h"
+#include <QMessageBox>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->analyzeButton, &QPushButton::clicked, this, &MainWindow::on_analyzeButton_clicked);
+    connect(ui->suggestButton, &QPushButton::clicked, this, &MainWindow::on_suggestButton_clicked);
 }
 
 MainWindow::~MainWindow()
@@ -14,9 +17,28 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
+
+void MainWindow::on_analyzeButton_clicked()
 {
-    TrieConcordance frequency;
+    QString text = ui->inputTextEdit->toPlainText().trimmed();
+    //wordMap = Map();
+    trie = TrieConcordance();
+
+    if (text.isEmpty()) {
+        QMessageBox::warning(this, "Error", "Please enter some text first.");
+        return;
+    }
+
+}
+void MainWindow::on_suggestButton_clicked()
+{
+    if (mostFrequentWord.isEmpty()) {
+        QMessageBox::warning(this, "Error", "Analyze text first.");
+        return;
+    }
+
+    QString text = ui->inputTextEdit->toPlainText();
+    string key = mostFrequentWord.toStdString();
 
 }
 
