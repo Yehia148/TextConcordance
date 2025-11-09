a177d6cba0924120df252779228f518ad9199083
Yehia148 <yahialsayed@aucegypt.edu>
2025-11-09 21:39:10 +0200

    Update mainwindow.cpp

--- UIForTextProject/mainwindow.cpp
+++ UIForTextProject/mainwindow.cpp
@@ -3,6 +3,7 @@
 #include "..\TrieConcordance.h"
 //#include "..\map.h"
 #include <QMessageBox>
+#include <QRegularExpression>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -20,14 +21,34 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_analyzeButton_clicked()
 {
-    QString text = ui->inputTextEdit->toPlainText().trimmed();
     //wordMap = Map();
     trie = TrieConcordance();
+    QString text = ui->inputTextEdit->toPlainText().trimmed();
+
 
     if (text.isEmpty()) {
         QMessageBox::warning(this, "Error", "Please enter some text first.");
         return;
     }
+    QStringList words = text.split(QRegularExpression("\\W+"), Qt::SkipEmptyParts);
+
+
+    for (int i = 0; i < words.size(); ++i) {
+        QString w = words[i].toLower();
+       // wordMap.put_word(w.toStdString());
+
+        QString next = (i + 1 < words.size()) ? words[i + 1].toLower() : "";
+        trie.insert(w.toStdString(), next.toStdString());
+    }
+
+    //vector<pair<string, int>> allWords = wordMap.sort_by_frequency();
+    //if (allWords.empty()) {
+   //     QMessageBox::information(this, "Info", "No words found.");
+   //     return;
+  //  }
+
+   // mostFrequentWord = QString::fromStdString(allWords.back().first);
+   // ui->frequentWordLabel->setText(mostFrequentWord);
 
 }
 void MainWindow::on_suggestButton_clicked()
